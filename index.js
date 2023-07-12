a850c4e91b7382371e2adcf5c14ce7a7a8385edc
nakib1948 <hayathossain.jtp@gmail.com>
2023-07-13 02:33:16 +0600

    all toys data loaded

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxayaa3.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,18 +25,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const coffeeCollection = client.db("ToysStore").collection("AllToys");
+    const toysCollection = client.db("ToysStore").collection("AllToys");
 
-   
+    app.get("/alltoys", async (req, res) => {
+        const cursor = toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
    
-    //await client.close();
   }
 }
 run().catch(console.dir);
