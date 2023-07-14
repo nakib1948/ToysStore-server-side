f88f4d435f00e30b8bb7e8b6e1e449cd6398b2f7
nakib1948 <hayathossain.jtp@gmail.com>
2023-07-15 02:51:42 +0600

    toy delete functionality added

--- index.js
+++ index.js
@@ -82,6 +82,15 @@ async function run() {
     
        })
 
+       app.delete('/toydelete/:id',async(req,res)=>{
+        const id=req.params.id
+        const query={_id: new ObjectId(id)}
+
+        const result=await toysCollection.deleteOne(query)
+        res.send(result)
+    })
+
+
 
 
     await client.db("admin").command({ ping: 1 });
