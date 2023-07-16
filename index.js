7b40a20417018f0da559068cf9de550d0e47092a
nakib1948 <hayathossain.jtp@gmail.com>
2023-07-17 03:32:13 +0600

    jwttoken added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
-
+const jwt = require("jsonwebtoken");
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,12 +20,40 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) => {
+  
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const toysCollection = client.db("ToysStore").collection("AllToys");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ token });
+    });
+
 
     app.get("/alltoys", async (req, res) => {
         const cursor = toysCollection.find();
@@ -46,7 +74,15 @@ async function run() {
         res.send(result);
       });
 
-      app.get("/mytoys", async (req, res) => {
+      app.get("/mytoys", verifyJWT, async (req, res) => {
+
+   
+       const decoded=req.decoded;
+      
+       if(decoded.email!==req.query.email)
+       {
+           return res.status(403).send({error:1,message:'forbidden access'})
+       }
         let query = {};
         if (req.query?.email) {
           query = { selleremail: req.query.email };
@@ -56,8 +92,14 @@ async function run() {
         res.send(result);
       });
 
-      app.post("/mytoys", async (req, res) => {
-
+      app.post("/mytoys", verifyJWT, async (req, res) => {
+        
+        const decoded=req.decoded;
+        
+        if(decoded.email!==req.query.email)
+        {
+            return res.status(403).send({error:1,message:'forbidden access'})
+        }
         const {text}=req.body
         let query = {};
         if (req.query?.email) {
